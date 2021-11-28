2169d354073c5c9aad270b6ddfbe5e04226f64da
D Paul <46006930+Selenium-H@users.noreply.github.com>
2021-11-29 04:14:53 +0530

    Improved support for GNOME 40+
    Rearranges app grid icons on reloading. Improved fix for issue #11

--- app-grid-tweaks@Selenium-H/extension_40.js
+++ app-grid-tweaks@Selenium-H/extension_40.js
@@ -1,6 +1,6 @@
 /*
 
-Version 3.03
+Version 3.04
 ============
  
 */
@@ -54,13 +54,10 @@ class AppGridTweaks {
 
   startAppGridTweaks() {
  
-    [this.iconSize, this.fRows, this.fColumns, this.sidePadding, OverviewControls.SIDE_CONTROLS_ANIMATION_TIME, IconGrid.PAGE_SWITCH_TIME] = [this.prefs.get_int("appgrid-icon-size"), this.prefs.get_int("folder-max-rows"), this.prefs.get_int("folder-max-columns"), this.prefs.get_int("side-padding"), this.prefs.get_int("open-animation-time"), this.prefs.get_int("page-switch-animation-time")];
-    [IconGrid.IconSize.LARGE, IconGrid.IconSize.MEDIUM, IconGrid.IconSize.SMALL, IconGrid.IconSize.TINY] = [this.iconSize, this.iconSize, this.iconSize, this.iconSize];
-
-    let appGrid = MainAppDisplay._grid; 
-    appGrid.style = "font-size: "+this.prefs.get_double("app-icon-font-size")+"px;"+this.prefs.get_string("label-style");  
-    appGrid = appGrid._gridModes;
-    [appGrid[0].rows, appGrid[0].columns] = [this.prefs.get_int("appgrid-max-rows"), this.prefs.get_int("appgrid-max-columns")];
+    let appGrid = MainAppDisplay._grid;
+    
+    [appGrid.style, appGrid._gridModes[0].rows, appGrid._gridModes[0].columns, this.iconSize, this.fRows, this.fColumns, this.sidePadding, OverviewControls.SIDE_CONTROLS_ANIMATION_TIME, IconGrid.PAGE_SWITCH_TIME] = ["font-size: "+this.prefs.get_double("app-icon-font-size")+"px;"+this.prefs.get_string("label-style"), this.prefs.get_int("appgrid-max-rows"), this.prefs.get_int("appgrid-max-columns"), this.prefs.get_int("appgrid-icon-size"), this.prefs.get_int("folder-max-rows"), this.prefs.get_int("folder-max-columns"), this.prefs.get_int("side-padding"), this.prefs.get_int("open-animation-time"), this.prefs.get_int("page-switch-animation-time")];       
+    [IconGrid.IconSize.LARGE, IconGrid.IconSize.MEDIUM, IconGrid.IconSize.SMALL, IconGrid.IconSize.TINY] = [this.iconSize, this.iconSize, this.iconSize, this.iconSize];   
       
     this.defaultAdaptToSizeFunction = MainAppDisplay.adaptToSize;
     MainAppDisplay.adaptToSize = this.overriddenAdaptToSizeFunction;     
@@ -71,19 +68,41 @@ class AppGridTweaks {
      
     this.reloadingSig = MainAppDisplay.connect("view-loaded", ()=> {
       this.applyFolderViewChanges();  
+      this.updatePages();
     });   
-  
+
+    appGrid._currentMode = 0; 
+    appGrid.layout_manager.rows_per_page    = appGrid._gridModes[0].rows;
+    appGrid.layout_manager.columns_per_page = appGrid._gridModes[0].columns;
+    appGrid.layout_manager.rowsPerPage      = appGrid._gridModes[0].rows;
+    appGrid.layout_manager.columnsPerPage   = appGrid._gridModes[0].columns;
+    if(appGrid.layout_manager._iconSize != this.iconSize) {
+      appGrid.layout_manager._pageWidth--; // To trigger execution of MainAppDisplay._grid.adaptToSize(pageWidth, pageHeight) which only executes when page width is changed. This is done to update Iconsize.
+    }
+    
+    this.updatePages();
     MainAppDisplay._redisplay();
 
   }
   
+  updatePages() {  
+  
+    for(let i = 0; i < MainAppDisplay._grid.layout_manager._pages.length; i++)
+      MainAppDisplay._grid.layout_manager._fillItemVacancies(i);   
+    MainAppDisplay._grid.layout_manager._updatePages();
+    for(let i = 0; i < MainAppDisplay._grid.layout_manager._pages.length; i++)
+      MainAppDisplay._grid.layout_manager._fillItemVacancies(i);
+      
+  }
+  
   undoChanges() {
     
-    let appGridModes = MainAppDisplay._grid._gridModes; 
+    let appGrid = MainAppDisplay._grid; 
+    appGrid.layout_manager._pageWidth--;
     MainAppDisplay.disconnect(this.reloadingSig);
     MainAppDisplay.adaptToSize = this.defaultAdaptToSizeFunction;  
-    [IconGrid.IconSize.LARGE, IconGrid.IconSize.MEDIUM, IconGrid.IconSize.SMALL, IconGrid.IconSize.TINY, OverviewControls.SIDE_CONTROLS_ANIMATION_TIME, IconGrid.PAGE_SWITCH_TIME, appGridModes[0].rows, appGridModes[0].columns] = [96, 64, 32, 16, 250, 300, 8, 3];
-
+    [IconGrid.IconSize.LARGE, IconGrid.IconSize.MEDIUM, IconGrid.IconSize.SMALL, IconGrid.IconSize.TINY, OverviewControls.SIDE_CONTROLS_ANIMATION_TIME, IconGrid.PAGE_SWITCH_TIME, appGrid._gridModes[0].rows, appGrid._gridModes[0].columns] = [96, 64, 32, 16, 250, 300, 8, 3];
+    MainAppDisplay.adaptToSize(appGrid.layout_manager._pageWidth, appGrid.layout_manager._pageHeight);
     MainAppDisplay._folderIcons.forEach(icon => {
       icon.view._grid.layout_manager.rows_per_page    = 3;
       icon.view._grid.layout_manager.columns_per_page = 3;
@@ -92,8 +111,9 @@ class AppGridTweaks {
       icon._dialog.child.style = "";
       icon.view._redisplay();
     });
-    MainAppDisplay._grid.style = "";   
-    MainAppDisplay._redisplay();   
+    appGrid.style = "";   
+    this.updatePages();  
+    MainAppDisplay._redisplay(); 
     
   }
   
@@ -102,10 +122,6 @@ class AppGridTweaks {
     const [, indicatorHeight] = this._pageIndicators.get_preferred_height(-1);
     height -= indicatorHeight;
  
-    MainAppDisplay._grid._currentMode = 0; 
-    MainAppDisplay._grid.layout_manager.rows_per_page = MainAppDisplay._grid._gridModes[0].rows;
-    MainAppDisplay._grid.layout_manager.columns_per_page = MainAppDisplay._grid._gridModes[0].columns;
-
     let box = new Clutter.ActorBox({
       x2: width,
       y2: height,
@@ -119,29 +135,25 @@ class AppGridTweaks {
     const availHeight = box.get_height();
 
     let pageHeight = availHeight;
-    let padding    = appGridTweaker.sidePadding;
-    let pageWidth  = availWidth-padding*2;
+    let hPadding   = appGridTweaker.sidePadding;
+    let pageWidth  = availWidth-hPadding*2;
 
-    MainAppDisplay._grid.layout_manager._pageWidth--; // MainAppDisplay._grid.adaptToSize(pageWidth, pageHeight) only executes when page width is changed. This is done to update Iconsize.
     MainAppDisplay._grid.adaptToSize(pageWidth, pageHeight);
 
-    const leftPadding = padding;
-    const rightPadding = padding;
-    const topPadding = Math.floor((availHeight - MainAppDisplay._grid.layout_manager.pageHeight) / 2);
-    const bottomPadding = Math.ceil((availHeight - MainAppDisplay._grid.layout_manager.pageHeight) / 2);
+    const vPadding = Math.floor((availHeight - MainAppDisplay._grid.layout_manager.pageHeight) / 2);
 
     MainAppDisplay._scrollView.content_padding = new Clutter.Margin({
-      left: leftPadding,
-      right: rightPadding,
-      top: topPadding,
-      bottom: bottomPadding,
+      left: hPadding,
+      right: hPadding,
+      top: vPadding,
+      bottom: vPadding,
     });
 
     MainAppDisplay._availWidth = availWidth;
     MainAppDisplay._availHeight = availHeight;
 
-    MainAppDisplay._pageIndicatorOffset = leftPadding;
-    MainAppDisplay._pageArrowOffset = Math.max(leftPadding - 80/*PAGE_PREVIEW_MAX_ARROW_OFFSET*/, 0);
+    MainAppDisplay._pageIndicatorOffset = hPadding;
+    MainAppDisplay._pageArrowOffset = Math.max(hPadding - 80/*PAGE_PREVIEW_MAX_ARROW_OFFSET*/, 0);
   
   }  
     
